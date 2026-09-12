1fb74cec21e2fb4230be7fe4c8d9abec2664f76c
Tanishk <sudanitanishk2003@gmail.com>
2026-09-12 13:27:29 +0530

    Configure modern Flutter Android runner and verified release APK build

--- android/app/proguard-rules.pro
+++
@@ -1,3 +0,0 @@
--keep class com.tanishk.aibot.** { *; }
--keepattributes Signature
--keepattributes *Annotation*
---
+++ android/app/src/main/kotlin/com/tanishk/tanishks_ai_bot/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.tanishk.tanishks_ai_bot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
